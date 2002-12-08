44dfc43fde047c1d6f8cfe13c91debaa7ea37fa8
Jim Evins <evins@snaught.com>
2002-12-08 04:19:56 +0000

    gl_window_new_from_file() no longer ignores abs_filename.
    git-svn-id: https://glabels.svn.sourceforge.net/svnroot/glabels/trunk@201 f5e0f49d-192f-0410-a22d-a8d8700d0965

--- glabels2/src/window.c
+++ glabels2/src/window.c
@@ -277,7 +277,7 @@ gl_window_new_from_file (const gchar *filename)
 	window = GL_WINDOW (gl_window_new ());
 
 	abs_filename = gl_util_make_absolute (filename);
-	label = gl_xml_label_open (filename, &status);
+	label = gl_xml_label_open (abs_filename, &status);
 	g_free (abs_filename);
 
 	gl_window_set_label (window, label);
