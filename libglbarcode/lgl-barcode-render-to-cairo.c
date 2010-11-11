45b59e5b4316338dde419f17a3fdb88575b6de02
Jim Evins <evins@snaught.com>
2010-11-10 22:16:24 -0500

    Update licensing information.
    - Docs are now licensed by the CC Attribution-Share Alike 3.0 Unported license
    - Libglbarcode is relicensed by the LGPL3+

--- libglbarcode/lgl-barcode-render-to-cairo.c
+++ libglbarcode/lgl-barcode-render-to-cairo.c
@@ -2,20 +2,20 @@
  *  lgl-barcode-render-to-cairo.c
  *  Copyright (C) 2010  Jim Evins <evins@snaught.com>.
  *
- *  This file is part of gLabels.
+ *  This file is part of libglbarcode.
  *
- *  gLabels is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
+ *  libglbarcode is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  gLabels is distributed in the hope that it will be useful,
+ *  libglbarcode is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ *  GNU Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with gLabels.  If not, see <http://www.gnu.org/licenses/>.
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with libglbarcode.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <config.h>
